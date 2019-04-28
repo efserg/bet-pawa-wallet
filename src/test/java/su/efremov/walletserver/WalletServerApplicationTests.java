11fcfdc43b422dcef69b882f2a70578c17121623
sergey.efremov <sergey.efremov@smartbics.com>
2019-04-28 14:14:28 +0300

    Init project structure

---
+++ src/test/java/su/efremov/walletserver/WalletServerApplicationTests.java
@@ -0,0 +1,16 @@
+package su.efremov.walletserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalletServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
